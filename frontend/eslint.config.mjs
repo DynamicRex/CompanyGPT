2ece5ab582a33ee929f10de64608389c98ebf617
Cyrus Kandilat <cyruskandilat@gmail.com>
2024-09-02 10:36:31 +0400

    Version 1.11 - fixed the eslint issue, global jest is now included.

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -6,7 +6,7 @@ export default [
   {
     files: ["**/*.{js,mjs,cjs,jsx}"],
     languageOptions: {
-      globals: { ...globals.browser, ...globals.node },
+      globals: { ...globals.browser, ...globals.node,...globals.jest },
     },
     settings: {
       react: {
